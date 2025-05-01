72e476cc425bde06a63d3d07497f96557427918c
InakiGT <yarimfuego@gmail.com>
2025-05-01 16:33:36 -0600

    [ADDED] CookieStore Polyfill

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { cookieStore } from 'cookie-store';
 
 export const getToken = async (): Promise<any> => {
   return await cookieStore.get('jwt')
 }
 
 export const setToken = async ( token: string ): Promise<any> => {
-  await cookieStore.set({ name: 'jwt', value: token })
+  await cookieStore.set('jwt', token)
 }
 
 export const removeToken = async () => {
