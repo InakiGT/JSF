b2b5d1e1cf6fc80bf1b4bc0f9e004e772d9e5dbf
InakiGT <yarimfuego@gmail.com>
2025-07-24 13:30:43 -0600

    [UPDATED] Base URL of the APP to /materiales instead of /dcni

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/dcni/'>
+    <BrowserRouter basename='/materiales/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
